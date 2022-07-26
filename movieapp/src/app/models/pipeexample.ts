f4b1cf4971d7eeb8fc7c4c8172c27492db263b1a
Nezir Turhallı <nzr.trhll@gmail.com>
2022-07-26 10:12:45 +0300

    pipeII

---
+++ movieapp/src/app/models/pipeexample.ts
@@ -0,0 +1,39 @@
+// <!-- --------------------------------------------------- -->
+
+// <!-- PIPES EXAMPLES -->
+
+// <!-- <h4>lower-upper-title pipe</h4>
+// <p>{{'Samsung 20'}}</p>
+// <p>{{'Samsung 20' | lowercase}}</p>
+// <p>{{'Samsung 20' | uppercase}}</p>
+// <p>{{'Samsung 20' | titlecase}}</p>
+
+// <br><br>
+
+// <h4>Date Pipe</h4>
+// <p>{{today | date}}</p>
+// <p>{{today | date:'fullDate'}}</p>
+// <p>{{today | date:'medium'}}</p>
+// <p>{{today | date:'h:mm:ss'}}</p> -->
+
+// <!-- <br />
+// <h4>Decimal Pipe</h4>
+
+// <p>{{2.23435643 | number}}</p>
+// <p>{{2.23435643 | number:'2.0-3'}}</p>
+// <p>{{2.23435643 | number: '4.1-7'}}</p>
+
+// <br />
+// <h4>Currency Pipe</h4>
+
+// <p>{{395 | currency}}</p>
+// <p>{{395 | currency:'EUR'}}</p>
+// <p>{{395 | currency: 'TRY'}}</p>
+
+
+// <br />
+// <h4>Percent Pipe</h4>
+// <p>{{0.259 | percent}}</p>
+// <p>{{0.0259 | percent}}</p>
+// <p>{{1.26 | percent:'2.2-4'}}</p>
+// <p>{{3/4 | percent:'2.2-4'}}</p> -->
